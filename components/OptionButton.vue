2f21a4f082a60467a409f0b810c0dda9fcb05a5e
Zhengyi Shen <zhengyishen1@gmail.com>
2024-10-28 23:57:21 -0700

    update option buttons

--- components/OptionButton.vue
+++ components/OptionButton.vue
@@ -4,35 +4,31 @@
       variant="outline"
       @click="handleClick"
       class="flex-1 justify-start text-left h-auto py-3 px-4 relative"
-      :class="{ 'text-gray-400': showStrikethrough }">
+      :class="{ 'text-gray-400': showStrikethrough && isStrikethrough }">
       <span
-        class="mr-4 inline-flex items-center justify-center h-6 w-6 rounded-full"
-        :class="{
-          'bg-gray-200': !showStrikethrough,
-          'bg-gray-100': showStrikethrough,
-        }"
+        class="mr-4 inline-flex items-center justify-center h-6 w-6 rounded-full bg-gray-100"
         >{{ optionId }}</span
       >
       {{ optionText }}
       <div
-        v-if="showStrikethrough"
+        v-if="showStrikethrough && isStrikethrough"
         class="absolute inset-0 flex items-center"
         aria-hidden="true">
         <div class="w-full border-t border-gray-300"></div>
       </div>
     </Button>
-    <template v-if="isStrikethrough">
+    <div v-if="isStrikethrough" class="w-12 flex items-center justify-center">
       <CircleOff
         v-if="!showStrikethrough"
         @click="toggleStrikethrough"
-        class="h-5 w-5 flex-shrink-0" />
+        class="h-5 w-5 flex-shrink-0 cursor-pointer" />
       <button
         v-else
         @click="toggleStrikethrough"
         class="text-sm text-gray-600 underline">
         Undo
       </button>
-    </template>
+    </div>
   </div>
 </template>
 
@@ -60,14 +56,14 @@ defineProps({
   },
 });
 
-const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(["click", "update:modelValue"]);
 
 const toggleStrikethrough = () => {
   showStrikethrough.value = !showStrikethrough.value;
 };
 
 const handleClick = () => {
-  toggleStrikethrough();
+  showStrikethrough.value = false;
   emit("click");
 };
 </script>
